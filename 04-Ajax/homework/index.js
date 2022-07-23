5a749649d6aaf442e98fc7ffc9905cb2ece81a66
Joseph Godoy <josephgodoy.t@gmail.com>
2022-07-23 09:35:26 -0600

    third commit

--- 04-Ajax/homework/index.js
+++ 04-Ajax/homework/index.js
@@ -1,30 +1,49 @@
-$( "#boton" ).click(function() {
-    
-    $.get('http://localhost:5000/amigos/', function(data){
-        for (let i = 0; i < data.length; i++){
-            let nombre = document.createElement('li')
-            nombre.setAttribute('id', `${i + 1}`)
-            nombre.innerText = data[i].name
-            let lugar = document.querySelector('#lista')
-            lugar.appendChild(nombre)
+let url = 'http://localhost:5000/amigos/'
+let lista = document.querySelector('#lista')
+let inputVer = document.querySelector('#input')
+let inputDelete = document.querySelector('#inputDelete')
+let espacio = document.querySelector('#amigo')
+let save = document.querySelector('#inputDelete')
+let Exitoso = document.querySelector('#success')
+let mensaje = document.createElement('span')
 
-        }
+
+
+$('#boton').click(verAmigos)
+$('#search').click(verUnAmigo)
+$('#delete').click(borrarUnAmigo)
+
+function verAmigos() {
+  lista.innerHTML = ''
+  fetch(url)
+    .then(res => res.json())
+    .then(amigos => {
+      if (amigos.length === 0) {
+        mensaje.innerText = 'no hay mas amigos'
+        lista.appendChild(mensaje)
+      } else {
+        amigos.forEach(XD => {
+          lista.innerHTML = lista.innerHTML + `<li>${XD.name}</li>`
+        })
+      }
     })
-  });
+};
 
-  $('#search').click(function(){
-    let save = document.querySelector('#input')
-    $.get(`http://localhost:5000/amigos/${save.value}`, function(data){
-        let text = document.querySelector('#amigo')
-        text.innerText = data.name;
+function verUnAmigo() { 
+    fetch(url + inputVer.value)
+    .then(res => res.json())
+    .then(amigo => {
+        espacio.innerText = amigo.name
+        inputVer.value = '';   
+    })      
+}
+
+function borrarUnAmigo() {
+  fetch(url + inputDelete.value, { method: 'DELETE' })
+    .then(res => res.json())
+    .then(()=> {
+        Exitoso.innerText = 'Se elimino con exito'
+        inputDelete.value = ''
+        verAmigos();
     })
-  })
-
-  $('#delete').click(function(){
-    let save = document.querySelector('#inputDelete')
-    $('#'+save.value).remove()
-    let mensaje = document.createElement('span')
-    let lugar = document.querySelector('#success')
-    mensaje.innerText = 'tu amigo se borro con exito'
-    lugar.appendChild(mensaje)
-  })
+}
